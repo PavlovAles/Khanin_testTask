b96de62c64a239a04194fa15b1af854b696254d4
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-06-21 11:00:06 +0300

    feat: randomizeColor of card

--- components/Card.js
+++ components/Card.js
@@ -8,6 +8,7 @@ export default class Card {
     this._element = this._getTemplate();
     this._element.querySelector('.list__card-title').textContent = this._data.theme;
     this._element.querySelector('.list__card-text').textContent = this._data.sourceText;
+    this._element.style.backgroundColor = `rgb(${this._randomizeColor()})`
     return this._element;
   }
 
@@ -16,5 +17,14 @@ export default class Card {
       .querySelector(this._templateSelector)
       .content.querySelector(".list__card")
       .cloneNode(true);
+    }
+
+  _randomizeColor(){
+    const codes = [102, 204];
+    const color = [0,0,0];
+    color[Math.floor(Math.random() * 2)] = 102;
+    color[Math.floor(Math.random() * 2)] = 204;
+    console.log(color)
+    return color.join();
   }
 }
